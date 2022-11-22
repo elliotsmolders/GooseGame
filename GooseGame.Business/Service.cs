31407189dc0814171cf0b350a598882ff873705c
Bart Roels <roelsbart@gmail.com>
2022-11-22 08:27:29 +0100

    not finished

---
+++ GooseGame.Business/Service.cs
@@ -0,0 +1,32 @@
+﻿using GooseGame.DAL.Entities;
+using GooseGame.DAL.Repositories;
+using SQLitePCL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GooseGame.Business
+{
+    public class Service
+    {
+        private GameRepository _gameRepo;
+        private PlayerRepository _playerRepo;
+        private GamePlayerRepository _gamePlayerRepository;
+
+        public Service()
+        {
+            _gameRepo = new GameRepository();
+            _playerRepo = new PlayerRepository();
+            _gamePlayerRepository = new GamePlayerRepository();
+        }
+
+        public void GetAllData()
+        {
+            //_gameRepo
+            //_playerRepo
+            _gamePlayerRepository._ctx.
+        }
+    }
+}
---
+++ GooseGame.DAL/Entities/Hook.cs
@@ -0,0 +1,13 @@
+﻿using GooseGame.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GooseGame.DAL.Entities
+{
+    public class Hook : BaseEntity
+    {
+    }
+}
