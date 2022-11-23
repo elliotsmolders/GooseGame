0a04f13839c760d9897708f1b74f4aeceb02b49a
Bart Roels <roelsbart@gmail.com>
2022-11-23 12:33:02 +0100

    Fix DB file add added repo classes

---
+++ GooseGame.DAL/Repositories/GameRepository.cs
@@ -0,0 +1,13 @@
+﻿using GooseGame.DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GooseGame.DAL.Repositories
+{
+    public class GameRepository : BaseRepository<GameEntity>
+    {
+    }
+}
